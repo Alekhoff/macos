ebadaed206b7a9d1681a2cafbae2ce28de3c01fc
Alekhoff <alekhoff@gmail.com>
2022-08-05 14:15:10 +0200

    init

---
+++ brewfile.sh
@@ -0,0 +1,75 @@
+# Taps
+tap 'homebrew/bundle'
+tap 'homebrew/cask'
+tap 'homebrew/cask-drivers'
+tap 'homebrew/cask-fonts'
+tap 'homebrew/cask-versions'
+tap 'homebrew/core'
+tap 'homebrew/services'
+
+# Formulae
+brew 'starship'
+brew 'python'
+brew 'coreutils'
+brew 'git'
+brew 'git-delta'
+brew 'gh'
+brew 'htmltest'
+brew 'node'
+brew 'speedtest-cli'
+brew 'bash'
+brew 'bash-completion'
+brew 'zsh'
+brew 'zsh-autosuggestions'
+brew 'zsh-syntax-highlighting'
+
+#Casks
+cask 'alt-tab'
+cask 'coconutbattery'
+cask 'google-chrome'
+cask 'raycast'
+cask 'telegram-desktop'
+cask 'spotify'
+cask 'discord'
+cask 'shottr'
+cask 'betterdisplay'
+cask 'github'
+cask 'aldente'
+cask 'iterm2'
+cask 'appcleaner'
+cask 'cyberduck'
+cask 'handbrake'
+cask 'firefox'
+cask 'iina'
+cask 'transmission'
+cask 'keka'
+cask 'macdown'
+cask 'raindropio'
+cask 'visual-studio-code'
+cask 'teamviewer'
+cask 'steam'
+cask 'private-internet-access'
+
+# Fonts
+cask 'font-fira-code'
+cask 'font-hasklig'
+cask 'font-inter'
+cask 'font-jetbrains-mono'
+cask 'font-roboto-mono-for-powerline'
+cask 'font-source-code-pro'
+
+# Mac App Store Applications
+mas 'Amphetamine', id: 937984704
+mas 'Bitwarden', id: 1352778147
+mas 'Keynote', id: 409183694
+mas 'Magnet', id: 441258766
+mas 'Numbers', id: 409203825
+mas 'Pages', id: 409201541
+man 'Snippetslab', id: 1006087419
+man 'Velja', id: 1607635845
+man 'Dropover', id: 1355679052
+man 'Upnote', id: 1398373917
+man 'Pixelmator Pro', id: 1289583905
+man 'Hidden Bar', id: 1452453066
+man 'Paprika Recipe Manager', id: 1303222628
+man 'Magnet', id: 441258766
---
+++ scripts/config-macos.sh
@@ -0,0 +1,196 @@
+#!/usr/bin/env bash
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Switch to dark mode
+osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to 1'
+
+
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
+
+# Finder: open everything in list view.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Finder: show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Finder: show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder: display full POSIX path as window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Dock: hide recent apps
+defaults write com.apple.dock show-recents -bool false
+
+# Dock: remove the auto-hiding delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Dock: remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Dock: automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# windows minimize on app icons
+defaults write com.apple.dock minimize-to-application -bool true
+# scaling effect on minimize
+defaults write com.apple.dock mineffect -string "scale"
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
+###############################################################################
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Key repeat: disable press-and-hold in favor of key repeat
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+###############################################################################
+# Screen                                                               #
+###############################################################################
+
+# Screenshot: create and set location
+mkdir -p ~/Pictures/Screenshots
+defaults write com.apple.screencapture location ~/Pictures/Screenshots
+
+# Enable HiDPI display modes (requires restart)
+sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+###############################################################################
+# Safari & WebKit                                                             #
+###############################################################################
+
+# Safari: show the full URL in the address bar (note: this still hides the scheme)
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+
+# Enable “Do Not Track”
+defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
+
+# Prevent Safari from opening ‘safe’ files automatically after downloading
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+# Privacy: don’t send search queries to Apple
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
+# Safari: enable the Develop menu and the Web Inspector
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+# Safari: add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+###############################################################################
+# Time Machine                                                                #
+###############################################################################
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+###############################################################################
+# Activity Monitor                                                            #
+###############################################################################
+
+# Show the main window when launching Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Visualize CPU usage in the Activity Monitor Dock icon
+defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+# Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
+
+###############################################################################
+# Address Book, Dashboard, iCal, TextEdit, and Disk Utility                   #
+###############################################################################
+
+# Use plain text mode for new TextEdit documents
+defaults write com.apple.TextEdit RichText -int 0
+
+# Open and save files as UTF-8 in TextEdit
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+###############################################################################
+# Mac App Store                                                               #
+###############################################################################
+
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Turn on app auto-update
+defaults write com.apple.commerce AutoUpdate -bool true
+
+###############################################################################
+# Photos                                                                      #
+###############################################################################
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+
+echo "MacOS config one. Some of these changes require a logout/restart to take effect."
